b8cc0316f64a76c3f9dd8162da4baca330bf7396
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2022-11-29 01:44:52 +0200

    Finished Team Join UI with fully responsive functionality

--- AugmenTableUnity/BoFUN/Assets/Scripts/MenuScripts/TeamCard.cs
+++ AugmenTableUnity/BoFUN/Assets/Scripts/MenuScripts/TeamCard.cs
@@ -5,19 +5,49 @@ using UnityEngine.UI;
 
 public class TeamCard : MonoBehaviour
 {
-    //public 
-    public Image backgroundImage;
+    //public
+    [Header("Component references needed for the card to work")]
+    public Image backgroundImageComponent;
+    public Image avatarImageComponent;
+    public GameObject playerEntry;
 
     public Color BackgroundColor
     {
-        get => backgroundImage.color;
-        set => backgroundImage.color = value;
+        get => backgroundImageComponent.color;
+        set => backgroundImageComponent.color = value;
+    }
+
+    public Sprite Avatar
+    {
+        get => avatarImageComponent.sprite;
+        set => avatarImageComponent.sprite = value;
+    }
+
+    private List<PlayerDescriptor> m_JoinedPlayers = new List<PlayerDescriptor>();
+
+    public void AddPlayer(PlayerDescriptor player)
+    {
+        m_JoinedPlayers.Add(player);
+    }
+
+    public bool RemovePlayer(int playerId)
+    {
+        //WebSocketSharp
+        for(int i=m_JoinedPlayers.Count-1; i>=0; i--)
+        {
+            if (m_JoinedPlayers[i].PlayerId == playerId)
+            {
+                m_JoinedPlayers.RemoveAt(i);
+                return true;
+            }
+        }
+        return false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!backgroundImage)
+        if (!backgroundImageComponent)
         {
             Debug.Log("Incorrect Team Card setup");
             return;
