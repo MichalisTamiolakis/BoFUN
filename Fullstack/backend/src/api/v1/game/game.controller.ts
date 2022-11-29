4905b554091fb31990af3604ebf266452e8f2298
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2022-11-29 18:09:59 +0200

    Revert alexandra's functions

--- Fullstack/backend/src/api/v1/game/game.controller.ts
+++ Fullstack/backend/src/api/v1/game/game.controller.ts
@@ -3,6 +3,7 @@ import { NotFound, BadRequest } from 'http-errors';
 
 import { IGame } from "../../../models/Game/game";
 import { ITeam } from '../../../models/Team/team';
+import { IPlayer } from '../../../models/Player/player';
 
 var currentGameModule = require('../../../models/Game/currentGame.module');
 var gameSettingsModule = require('../../../models/GameSettings/gameSettings.module');
@@ -11,8 +12,10 @@ export class Game{
     public applyRoutes():Router {
         const router = Router();
         
-        router.post('/create', this.createGame());
-
+        router
+        
+        .post('/create', this.createGame())
+        .post('/createPlayer', this.createPlayer)
 
         return router; 
 
@@ -58,4 +61,45 @@ export class Game{
             return res.sendStatus(200);
           };
     }
+
+    public createPlayer() {
+        return async (
+          req: Request,
+          res: Response,
+          next?: NextFunction
+        ): Promise<Response> => {
+          let player: IPlayer = {
+            id: new Date().getTime(),
+            username: "",
+            teamId: -1,
+            image: "",
+            positionId: req.body.positionId,
+          };
+          currentGameModule.game.players.push(player);
+          res.sendStatus(200);
+          return res.send(currentGameModule.game);
+        };
+      }
+    
+      public assignPlayerToTeam() {
+        return async (
+          req: Request,
+          res: Response,
+          next?: NextFunction
+        ): Promise<Response> => {
+          let players: Array<IPlayer> = currentGameModule.game.players;
+          let player = players.find(({ id }) => id === Number(req.params.id));
+          if (player !== undefined) {
+            player.teamId = req.body.teamId;
+          }
+          let teams: Array<ITeam> = currentGameModule.game.teams;
+          let chosenTeam = teams.find(({ id }) => id === Number(req.body.teamId));
+          if(chosenTeam !== undefined){
+            chosenTeam.members.push(Number(req.params.id));
+            res.sendStatus(200);
+            return res.send(currentGameModule.game);
+          }
+          return res.sendStatus(400);
+        };
+      }
 }
