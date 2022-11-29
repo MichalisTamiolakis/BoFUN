5b4004b522bb468f7485359c44965aba2417993e
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2022-11-29 17:37:59 +0200

    - Create game controller

--- Fullstack/backend/src/api/v1/game/game.controller.ts
+++ Fullstack/backend/src/api/v1/game/game.controller.ts
@@ -2,8 +2,10 @@ import { Request, Response, NextFunction, Router } from 'express';
 import { NotFound, BadRequest } from 'http-errors';
 
 import { IGame } from "../../../models/Game/game";
+import { ITeam } from '../../../models/Team/team';
 
 var currentGameModule = require('../../../models/Game/currentGame.module');
+var currentGameModule = require('../../../models/GameSettings/gameSettings.module');
 
 export class Game{
     public applyRoutes():Router {
@@ -18,6 +20,42 @@ export class Game{
 
     public createGame(){
         return async (req: Request, res: Response, next?: NextFunction): Promise<Response> => {
+            
+            let colors:Array<string> = availableTeamColors;
+
+            let teams:Array<ITeam> = 
+            [];
+
+            for (let i = 0; i < req.body.totalTeams; i++) {
+                let choice = Math.floor(Math.random()*colors.length);
+                let chosenColor = colors[choice];
+                colors =colors.splice(choice,1);
+
+                teams.push({
+                    id:i,
+                    name: "",
+                    image: "",
+                    members: [],
+                    color: chosenColor,
+                    sequence: []
+                })
+            }
+
+            currentGameModule.game = 
+            {
+                duration: req.body.duration,
+                totalPlayers: req.body.totalPlayers,
+                players:[],
+                teams: teams,
+                pantomime: req.body.pantomime,
+                pictionary: req.body.pictionary,
+                trivia: req.body.trivia,
+                sequence: [],
+                winningTeam: -1,
+                rounds: []
+            };
+            
+            
             return res.sendStatus(200);
           };
     }
---
+++ Fullstack/backend/src/models/GameSettings/gameSettings.module.ts
@@ -0,0 +1,10 @@
+
+var availableTeamColors:Array<string> = 
+[
+    "6096BA",
+    "A663CC",
+    "6BBF59",
+    "F2CA68"
+]
+
+exports.availableTeamColors = availableTeamColors;
