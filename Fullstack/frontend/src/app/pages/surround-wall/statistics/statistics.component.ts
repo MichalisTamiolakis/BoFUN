a51ec8e71f2ce1c84c9f525fd13cf12da9903e94
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2022-12-23 18:45:24 +0200

    Finished leap intergration in the surroundwall

--- Fullstack/frontend/src/app/pages/surround-wall/statistics/statistics.component.ts
+++ Fullstack/frontend/src/app/pages/surround-wall/statistics/statistics.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { map, Subscription, timer } from 'rxjs';
 import { GameService } from 'src/app/global/services/game.service';
+import { Gestures, LeapService } from 'src/app/global/services/leap.service';
 // import { ApexChart } from "apexcharts";
 
 @Component({
@@ -8,25 +10,67 @@ import { GameService } from 'src/app/global/services/game.service';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
-  chartOptions:any
-  teamsScores:any
-  gamesScores:any
-  playersScores:any
-  constructor(private gameService: GameService) { 
+    @ViewChild('column0ScrollableContainer', { static: true }) column0ScrollableContainer: ElementRef;
+    @ViewChild('column1ScrollableContainer', { static: true }) column1ScrollableContainer: ElementRef;
+    @ViewChild('column2ScrollableContainer', { static: true }) column2ScrollableContainer: ElementRef;
     
+    chartOptions:any
+    teamsScores:any
+    gamesScores:any
+    playersScores:any
 
-  }
+    public selectedColumn:number = 2;
 
-  ngOnInit(): void {
-    this.gameService.getTeamsScores().subscribe((result) => {
-      this.teamsScores = result
-    })
-    this.gameService.getGamesScores().subscribe((result) => {
-      this.gamesScores = result
-    })
-    this.gameService.getPlayersScores().subscribe((result) => {
-      this.playersScores = result
-    })
-  }
+    constructor(private gameService: GameService, public leapService:LeapService) {
+    }
+    timerSubscription: Subscription;
+    ngOnInit(): void {
 
+        this.gameService.getTeamsScores().subscribe((result) => {
+            this.teamsScores = result
+        })
+        this.gameService.getGamesScores().subscribe((result) => {
+            this.gamesScores = result
+        })
+        this.gameService.getPlayersScores().subscribe((result) => {
+            this.playersScores = result
+        })
+
+        this.leapService.gestureRecognizer().subscribe((gesture: Gestures) => {
+            
+            // Change column
+            if (gesture === Gestures.SWIPE_LEFT) {
+                // this.selectedColumn = Math.min(this.selectedColumn+1, 2);
+                this.changeFocusedColumn(-1);
+            }
+            else if (gesture === Gestures.SWIPE_RIGHT) {
+                this.changeFocusedColumn(+1);
+            }
+
+            // Scroll
+            else if(gesture === Gestures.SWIPE_UP){
+                this.scrollSelectedColumn(-120);
+            }
+            else if(gesture === Gestures.SWIPE_DOWN){
+                this.scrollSelectedColumn(+120);
+            }
+        });
+
+    }
+
+    private changeFocusedColumn(columnChange:number){
+        this.selectedColumn = Math.max(Math.min(this.selectedColumn + columnChange, 2),0);
+    }
+
+    private scrollSelectedColumn(amount:number){
+        if(this.selectedColumn === 0){
+            this.column0ScrollableContainer.nativeElement.scrollTop += amount;
+        }
+        else if(this.selectedColumn===1){
+            this.column1ScrollableContainer.nativeElement.scrollTop += amount;
+        }
+        else if(this.selectedColumn===2){
+            this.column2ScrollableContainer.nativeElement.scrollTop += amount;
+        }
+    }
 }
