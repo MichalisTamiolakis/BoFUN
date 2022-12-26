b5d01fcf75e18baad563553d3c90a07aa8015226
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2022-12-26 02:09:14 +0200

    Backend Timer api

--- Fullstack/backend/src/api/v1/round/round.controller.ts
+++ Fullstack/backend/src/api/v1/round/round.controller.ts
@@ -3,8 +3,11 @@ import { Request, Response, NextFunction, Router } from "express";
 import { Game } from "../../../models/Game/game";
 import { ITeam } from "../../../models/Team/team";
 import { IRound, MiniGame } from "../../../models/Round/round";
+import { DIContainer, SocketsService } from "../../../services";
 
 var currentGame: Game = require("../../../models/Game/currentGame.module").game;
+const socketService = DIContainer.get(SocketsService);
+
 
 export class Round {
     public applyRoutes(): Router {
@@ -109,13 +112,35 @@ export class Round {
         ): Promise<Response> => {
             let currentRound: IRound | undefined = currentGame.getCurrentRound();
             if (currentRound) {
-                currentRound.started = true;
+                
+                socketService.broadcast("CurrentRoundStarted", currentRound);
 
                 // TODO - Start Timer
+
+                // socketService.broadcast("TeamUpdated", JSON.stringify(chosenTeam));
+                this.StartTimer(currentRound);
                 
                 return res.send(currentRound);
             }
             return res.sendStatus(404);
         };
+    }
+
+    private StartTimer(round:IRound){
+        round.started = true;
+        let timer = setInterval(()=>{
+            if(round.ended || round.remainingTime<=0)
+            {
+                round.ended = true;
+                clearInterval(timer);
+                return;
+            }
+            
+            round.remainingTime -= 1;
+            socketService.broadcast("CurrentRoundTimerUpdated", round.remainingTime);
+
+        }, 1000);
+
+        // else return Promise.delay(1000).then(() => a());
     }
 }
