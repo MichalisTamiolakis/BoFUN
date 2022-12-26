8c15f44435cf583a433a0a7d2e532fc0f74a7e2e
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2022-12-26 02:19:18 +0200

    Entities augmentable

---
+++ AugmenTableUnity/BoFUN/Assets/Scripts/Entities/Pantomime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BoFUN.Entities
+{
+    class Pantomime
+    {
+        public int id = -1;
+        public string category = "";
+        public string task = "";
+
+        public static string ToJSON(Pantomime p)
+        {
+            return JsonUtility.ToJson(p);
+        }
+
+        public static Pantomime CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<Pantomime>(jsonString);
+        }
+    }
+}
---
+++ AugmenTableUnity/BoFUN/Assets/Scripts/Entities/Pictionary.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BoFUN.Entities
+{
+    class Pictionary
+    {
+        public int id=-1;
+        public int difficulty = 0;
+        public string task = "";
+
+        public static string ToJSON(Pictionary p)
+        {
+            return JsonUtility.ToJson(p);
+        }
+
+        public static Pictionary CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<Pictionary>(jsonString);
+        }
+    }
+}
