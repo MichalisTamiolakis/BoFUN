ab4a878a955e9027ca655a9cf5d1b88e7393ffe9
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2023-01-22 23:17:31 +0200

    Fixed surroundwall to work with less than 2 answers

--- Fullstack/frontend/src/app/components/smartphone/trivia-option/trivia-option.component.ts
+++ Fullstack/frontend/src/app/components/smartphone/trivia-option/trivia-option.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SocketsService } from 'src/app/global/services/sockets/sockets.service';
 
 @Component({
   selector: 'app-trivia-option',
@@ -11,18 +12,21 @@ export class TriviaOptionComponent implements OnInit {
   @Input('options') options: any;
   public chosen:boolean = false
   public alphabet: Array<string> = ["A","B","C", "D"]
-  constructor() { }
+  constructor(private sockets:SocketsService) { }
 
   ngOnInit(): void {
   }
 
   chooseAnswer(){
+
     for (let i = 0; i < this.options.length; i++) {
       if(i===this.index) continue;
       this.options[i] = false;
       
     }
     this.options[this.index] = !this.options[this.index];
+
+    this.sockets.publish("TriviaSelectedAnswerChanged", this.options[this.index]?this.index:-1);
   }
 
 }
